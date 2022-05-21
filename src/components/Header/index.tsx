86524d8a2cc2ffd817f7fee8a8feacba293f40eb
Juliano Firme <julianofirme1@gmail.com>
2022-05-21 20:14:18 -0300

    feat: sign in with github button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 
@@ -10,6 +11,8 @@ export function Header() {
           <a href="" className={styles.active}>Home</a>
           <a href="">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
